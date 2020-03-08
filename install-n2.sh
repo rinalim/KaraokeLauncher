198286589bc11dbecdcfaa9115e2545231f56ab7
rinalim <ooll00@naver.com>
2020-03-08 22:08:35 +0900

    Update install-n2.sh

--- install-n2.sh
+++ install-n2.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install ubuntu-mate-desktop -y
-sudo apt install vlc fcitx-hangul fcitx-config-gtk language-pack-ko -y
+sudo apt install wget vlc fcitx-hangul fcitx-config-gtk language-pack-ko -y
 sudo apt install openjdk-8-jdk -y
 
 wget http://download1510.mediafire.com/q9nzokcw8trg/g567nzri99pzifu/videoKaraoke-0.1.0-dist.zip
