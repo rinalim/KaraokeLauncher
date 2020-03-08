7e7c81f207350722780d05546b82b0be02113667
rinalim <ooll00@naver.com>
2020-03-08 21:21:07 +0900

    Update config-n2.sh

--- config-n2.sh
+++ config-n2.sh
@@ -1,6 +1,7 @@
+sudo service lightdm stop
 dconf write /org/mate/panel/toplevels/bottom/auto-hide true
 dconf write /org/mate/panel/toplevels/top/auto-hide true
 cp -r ./patch/fcitx/* /home/pigaming/.config/fcitx/
 mkdir /home/pigaming/.config/autostart
 cp ./patch/start.sh.desktop /home/pigaming/.config/autostart/
-sudo service lightdm restart
+sudo service lightdm start
