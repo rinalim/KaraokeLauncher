f1a5e5fb9fa2121de08e9f14271241bb6f1c6293
rinalim <ooll00@naver.com>
2020-03-09 01:20:25 +0900

    Update install-n2.sh

--- install-n2.sh
+++ install-n2.sh
@@ -3,11 +3,11 @@ sudo apt install ubuntu-mate-desktop -y
 sudo apt install wget vlc fcitx-hangul fcitx-config-gtk language-pack-ko -y
 sudo apt install openjdk-8-jdk -y
 
-if [ ! -f videoKaraoke-0.1.0-dist.zip ]; then
-  wget http://download1510.mediafire.com/q9nzokcw8trg/g567nzri99pzifu/videoKaraoke-0.1.0-dist.zip
-fi
+#if [ ! -f videoKaraoke-0.1.0-dist.zip ]; then
+#  wget http://download1510.mediafire.com/q9nzokcw8trg/g567nzri99pzifu/videoKaraoke-0.1.0-dist.zip
+#fi
 unzip -o videoKaraoke-0.1.0-dist.zip -d /home/pigaming/RetroArena/roms/kodi/videoKaraoke
-rm videoKaraoke-0.1.0-dist.zip
+#rm videoKaraoke-0.1.0-dist.zip
 
 cp -r ./patch/kodi /home/pigaming/RetroArena/roms/
 cp ./patch/setting.ini /home/pigaming/RetroArena/roms/kodi/videoKaraoke/
