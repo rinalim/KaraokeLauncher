bdd4061bba4336861261f1e58220e3346f61f6b2
rinalim <ooll00@naver.com>
2020-03-08 18:42:20 +0900

    fcitx patch

--- config-n2.sh
+++ config-n2.sh
@@ -1,6 +1,6 @@
 dconf write /org/mate/panel/toplevels/bottom/auto-hide true
 dconf write /org/mate/panel/toplevels/top/auto-hide true
-cp ./patch/config /home/pigaming/.config/fcitx/
+cp -r ./patch/fcitx/* /home/pigaming/.config/fcitx/
 mkdir /home/pigaming/.config/autostart
 cp ./patch/start.sh.desktop /home/pigaming/.config/autostart/
 sudo service lightdm restart
