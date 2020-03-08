2fc14760cb656405cd77ba13f739757d241c10da
rinalim <ooll00@naver.com>
2020-03-08 14:09:21 +0900

    Update +Start Karaoke.sh

--- patch/kodi/+Start Karaoke.sh
+++ patch/kodi/+Start Karaoke.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo service lightdm start
+/usr/bin/python /home/pigaming/KaraokeLauncher/joy-input.py /dev/input/js0
 /usr/bin/python /home/pigaming/KaraokeLauncher/run.py
 sudo service lightdm stop
