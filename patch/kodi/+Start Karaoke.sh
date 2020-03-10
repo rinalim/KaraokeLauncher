7db8a55cc87ac5d9bfc9260630e57b76a10a8314
rinalim <ooll00@naver.com>
2020-03-10 17:57:13 +0900

    Update +Start Karaoke.sh

--- patch/kodi/+Start Karaoke.sh
+++ patch/kodi/+Start Karaoke.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 sudo service lightdm start
+if [ ! -f /home/pigaming/.config/autostart/start.sh.desktop ]; then
+  sleep 10
+  sudo service lightdm stop
+  dconf write /org/mate/panel/toplevels/bottom/auto-hide true
+  dconf write /org/mate/panel/toplevels/top/auto-hide true
+  cp -r /home/pigaming/KaraokeLauncher/patch/fcitx/* /home/pigaming/.config/fcitx/
+  mkdir /home/pigaming/.config/autostart
+  cp /home/pigaming/KaraokeLauncher/patch/start.sh.desktop /home/pigaming/.config/autostart/start.sh.desktop
+  sudo service lightdm start
+fi
 /usr/bin/python /home/pigaming/KaraokeLauncher/joy-input.py /dev/input/js0 &
 /usr/bin/python /home/pigaming/KaraokeLauncher/run.py
 sudo service lightdm stop
