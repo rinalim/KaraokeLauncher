cb629a94f5c90e0177dcc42afb11c71a93ceaa78
rinalim <ooll00@naver.com>
2020-03-10 10:53:43 +0900

    Update +Start Karaoke.sh

--- patch/ports/+Start Karaoke.sh
+++ patch/ports/+Start Karaoke.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 sudo service lightdm start
+if [ ! -f /home/pi/.config/autostart/start.sh.desktop ]; then
+  sleep 10
+  sudo service lightdm stop
+  dconf write /org/mate/panel/toplevels/bottom/auto-hide true
+  dconf write /org/mate/panel/toplevels/top/auto-hide true
+  cp -r /home/pi/KaraokeLauncher/patch/fcitx/* /home/pi/.config/fcitx/
+  mkdir /home/pi/.config/autostart
+  cp /home/pi/KaraokeLauncher/patch/start.sh.desktop-pi3 /home/pi/.config/autostart/start.sh.desktop
+  sudo service lightdm start
+fi
 /usr/bin/python /home/pi/KaraokeLauncher/joy-input.py /dev/input/js0 &
 /usr/bin/python /home/pi/KaraokeLauncher/run.py
 sudo service lightdm stop
