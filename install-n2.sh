551a04d890c79409890ad7d7c606068a9ee6be6f
rinalim <ooll00@naver.com>
2020-03-11 01:23:09 +0900

    Update to 0.1.1

--- install-n2.sh
+++ install-n2.sh
@@ -6,7 +6,7 @@ sudo apt install openjdk-8-jdk -y
 #if [ ! -f videoKaraoke-0.1.0-dist.zip ]; then
 #  wget http://download1510.mediafire.com/q9nzokcw8trg/g567nzri99pzifu/videoKaraoke-0.1.0-dist.zip
 #fi
-unzip -o videoKaraoke-0.1.0-dist.zip -d /home/pigaming/RetroArena/roms/kodi/videoKaraoke
+unzip -o videoKaraoke-0.1.1-dist.zip -d /home/pigaming/RetroArena/roms/kodi/videoKaraoke
 #rm videoKaraoke-0.1.0-dist.zip
 
 cp -r ./patch/kodi /home/pigaming/RetroArena/roms/
--- install-pi3.sh
+++ install-pi3.sh
@@ -8,7 +8,7 @@ sudo apt install openjdk-8-jdk -y
 #if [ ! -f videoKaraoke-0.1.0-dist.zip ]; then
 #  wget http://download1510.mediafire.com/q9nzokcw8trg/g567nzri99pzifu/videoKaraoke-0.1.0-dist.zip
 #fi
-unzip -o videoKaraoke-0.1.0-dist.zip -d /home/pi/RetroPie/roms/ports/videoKaraoke
+unzip -o videoKaraoke-0.1.1-dist.zip -d /home/pi/RetroPie/roms/ports/videoKaraoke
 #rm videoKaraoke-0.1.0-dist.zip
 
 cp -r ./patch/ports /home/pi/RetroPie/roms/
